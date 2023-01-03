917689c9fd92908b14c859b49301edf351ee468d
daniil <daniilschmelzer@gmail.com>
2023-01-03 15:35:30 +0300

    initial commit

---
+++ src/test/java/postmanEchoTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+
+public class postmanEchoTest {
+    @Test
+    public void echoTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200).log().ifValidationFails()
+                .body("data", is("some data"));
+    }
+}
